7a7f864921c783151ff916629a29354eec0c9363
brinale <62750815+brinale@users.noreply.github.com>
2022-12-02 23:41:31 +0300

    done

--- app.py
+++ app.py
@@ -1,44 +1,52 @@
-from flask import Flask, g, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 import os
 import psycopg2
 import socket
+from sqlalchemy import create_engine, desc, Column, Integer, DateTime, inspect
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
-#counter = 0
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
+Base = declarative_base()
 
-class Counts(db.docker_db):
-  id = db.Column(db.Integer, primary_key=True)
-  amount = db.Column(db.Integer, nullable=False)
-  dateadded = db.Column(db.String, nullable=True)
+def recreate_database():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
 
-  def __init__(self, amount):
-    self.amount = amount
+class Counter(Base):
+    __tablename__ = 'counts'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    amount = Column(Integer)
+    date_added = Column(DateTime(timezone=True), server_default=func.now())
+    
+    def __repr__(self):
+        return self.amount
+    
+    def to_json(self):
+        return dict(amount=self.amount)
 
-def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='docker_db',
-                            user=os.environ['DB_USERNAME'],
-                            password=os.environ['DB_PASSWORD'])
-    return conn
+
+engine = create_engine(os.environ.get('DATABASE_URL'), pool_size=50)
+
+Session=sessionmaker(bind=engine)
+
+if not inspect(engine).has_table('counts'):
+    recreate_database()
+    c1 = Counter(amount=0)
+    session=Session()
+    session.add(c1)
+    session.commit()
+    session.close()
 
 @app.route("/")
 def show():
-   ## conn = get_db_connection()
-   ## cur = conn.cursor()
-   ## cur.execute('SELECT * FROM books;')
-   ## books = cur.fetchall()
-   ## cur.close()
-   ## conn.close()
-    items = []
-    for item in db.session.query(Counts).all():
-        del item.__dict__['_sa_instance_state']
-    items.append(item.__dict__)
-    #global counter
-    html = f"<p>{items[-1]}</p>"
+    session=Session()
+    item=session.query(Counter).order_by(desc(Counter.id)).first()
+    item=item.to_json().get('amount')
+    html = f"<p>{item}</p>"
+    session.close()
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 @app.route("/about")
@@ -48,11 +56,14 @@ def hello():
 
 @app.route("/stat")
 def incr():
-    global counter
-    db.session.add(Counts(1))
-    db.session.commit()
-    html = f"<p>{counter}</p>"
-    counter = counter + 1
+    session=Session()
+    item=session.query(Counter).order_by(desc(Counter.id)).first()
+    item=item.to_json().get('amount')
+    c = Counter(amount=item+1)
+    session.add(c)
+    session.commit()
+    session.close()
+    html = f"<p>{item}</p>"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
